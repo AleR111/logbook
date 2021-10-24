e3ec4ff52c894e01de118bfd6a43839633a9f9e3
Al <alexseier12345@gmail..com>
2021-10-24 22:52:08 +0300

    done header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,20 @@
+import classNames from "classnames"
+import styles from "./header.module.scss"
+import notice from "./img/notice.svg"
+
 export const Header = () => {
-  return <footer>f</footer>
+  return (
+    <div className={styles.nav}>
+      <div>100.00 ₽</div>
+      <div className={styles.boxCenter}>
+        <button className={classNames(styles.button, styles.notice)}>
+          <img src={notice} alt="notice" />
+        </button>
+        <button className={styles.button}>vikavishny</button>
+      </div>
+      <div>
+        <button className={styles.button}>Выйти</button>
+      </div>
+    </div>
+  )
 }
