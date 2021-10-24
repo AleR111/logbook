6e77563020b781e11236e8c40d8ff83c91c66e64
Al <alexseier12345@gmail..com>
2021-10-24 23:08:42 +0300

    create logbook

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,3 +1,15 @@
+import { Route, Switch } from "react-router-dom"
+import { Logbook } from "../logbook"
+
 export const Content = () => {
-  return <footer>f</footer>
+  return (
+    <Switch>
+      <Route path={"/account"}>account</Route>
+      <Route path={"/vps"}>vps</Route>
+      <Route path={"/domains"}>domains</Route>
+      <Route path={"/logbook"}>
+        <Logbook />
+      </Route>
+    </Switch>
+  )
 }
---
+++ src/components/logbook/Logbook.js
@@ -0,0 +1,10 @@
+// import styles from "./logbook.module.scss"
+
+export const Logbook = () => {
+  return (
+    <div>
+      <h4>Бортовой журнал</h4>
+      <h1>Бортовой журнал</h1>
+    </div>
+  )
+}
